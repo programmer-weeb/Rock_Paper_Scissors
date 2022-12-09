8d057b40a44ffcb5e6e7e629f590ed3c637f3212
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-09 20:22:10 +0200

    Finish game function
    play a 5 round game that keeps score and reports a winner or loser

--- app.js
+++ app.js
@@ -30,4 +30,22 @@ function playRound(playerSelection, computerSelection) {
 	else return "tie no one won";
 }
 
-console.log(playRound('paper', 'paper'));
+// Write a NEW function called game().
+// Call the playRound function inside of this one to
+// play a 5 round game that keeps score and reports a winner or loser at the end.
+
+function game() {
+	let userPoints = 0,
+		computerPoints = 0;
+	for (let i = 0; i < 5; i++) {
+		let userChoice = prompt("choose one");
+		let resultOfPlayRound = playRound(userChoice, getComputerChoice());
+		if (resultOfPlayRound == "player beats computer") userPoints++;
+		if (resultOfPlayRound == "computer beats player") computerPoints++;
+		console.log(resultOfPlayRound);
+	}
+	if (computerPoints > userPoints) console.log("Computer won");
+	else console.log("User won");
+}
+
+console.log(game());
