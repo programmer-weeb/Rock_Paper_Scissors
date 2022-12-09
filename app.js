63f90ad277abbafd69cf6c6e1c49a77c70199b38
Ahmed Melouk <84931803+programmer-weeb@users.noreply.github.com>
2022-12-09 19:35:00 +0200

    Finish getComputerChoice
    will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('hello from external js file');
+
+// Your game is going to play against the computer, so begin with a function called getComputerChoice
+// that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+
+function getComputerChoice() {
+    let values = ['Rock', 'Paper', 'Scissors'];
+    let randomIndex = Math.floor(Math.random() * values.length)
+    return values[randomIndex]
+
+}
